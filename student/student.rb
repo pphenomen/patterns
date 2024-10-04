e382193d3e0d789c5adbb22c2e0cd2c1f5cb8de1
pphenomen <dzamikhovz@gmail.com>
2024-10-05 00:03:07 +0300

    Lab2 - task1.7: phone number validate

--- student/student.rb
+++ student/student.rb
@@ -7,12 +7,26 @@ class Student
 		@first_name = hash_params[:first_name] || raise("Имя должно быть указано обязательно")
 		@patronymic = hash_params[:patronymic] || raise("Отчество должно быть указано обязательно")
 		@id = hash_params[:id]
-		@phone_number = hash_params[:phone_number]
+    	self.phone_number = hash_params[:phone_number] if hash_params[:phone_number]
 		@git = hash_params[:git]
 	end
 
+	def self.valid_phone_number?(phone_number)
+		if phone_number =~ (/\A\d{11}\z/)
+			true
+		else
+			false
+		end
+  	end
+
+  	def phone_number=(phone_number)
+    	if !Student.valid_phone_number?(phone_number) 
+    		raise "Номер телефона некорректный" 
+    	end
+    	@phone_number = phone_number
+  	end
+  	
 	def to_s
   		"ФИО: #{second_name} #{first_name} #{patronymic}, ID: #{id || 'не указан'}, Телефон: #{phone_number || 'не указан'}, Git: #{git || 'не указан'}"
 	end
-
 end
