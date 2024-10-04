0cae29e91b5851561e4f2c0484c6636636638125
pphenomen <dzamikhovz@gmail.com>
2024-10-04 18:20:10 +0300

    Lab2 - task1.6: args of the constructor to hash form

--- student/main.rb
+++ student/main.rb
@@ -1,8 +1,29 @@
 require './student.rb'
 
-student1 = Student.new("Ivanov", "Ivan", "Ivanovich", 1, "89287142211", "github.com/ivan")
-student2 = Student.new("Petrov", "Petr", "Petrovich", 2, "89674549878", "github.com/petr")
-student3 = Student.new("Sidorova", "Maria", "Ivanovna", 3, "89881234356", "github.com/maria")
+student1 = Student.new(
+	second_name: "Ivanov", 
+	first_name: "Ivan", 
+	patronymic: "Ivanovich",
+	id: 1,
+	phone_number: "89287142211",
+	git: "github.com/ivan"
+)
+student2 = Student.new(
+	second_name: "Petrov", 
+	first_name: "Petr", 
+	patronymic: "Petrovich", 
+	id: 2, 
+	phone_number: "89674549878", 
+	git: "github.com/petr"
+)
+student3 = Student.new(
+	second_name: "Sidorova", 
+	first_name: "Maria", 
+	patronymic: "Ivanovna", 
+	id: 3, 
+	phone_number: nil, 
+	git: nil
+)
 
 puts student1
 puts student2
--- student/student.rb
+++ student/student.rb
@@ -2,13 +2,13 @@ class Student
 	attr_accessor :id, :phone_number, :git
 	attr_reader :second_name, :first_name, :patronymic
 
-	def initialize(second_name, first_name, patronymic, id=nil, phone_number=nil, git=nil)
-		@second_name = second_name
-		@first_name = first_name
-		@patronymic = patronymic
-		@id = id
-		@phone_number = phone_number
-		@git = git
+	def initialize(hash_params = {})
+		@second_name = hash_params[:second_name] || raise("Фамилия должна быть указана обязательно")
+		@first_name = hash_params[:first_name] || raise("Имя должно быть указано обязательно")
+		@patronymic = hash_params[:patronymic] || raise("Отчество должно быть указано обязательно")
+		@id = hash_params[:id]
+		@phone_number = hash_params[:phone_number]
+		@git = hash_params[:git]
 	end
 
 	def to_s
