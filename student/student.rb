b75182c48fcebee1913f06e496178aff3788599c
pphenomen <dzamikhovz@gmail.com>
2024-10-04 16:18:19 +0300

    Lab2 - task1.3-5: added attr_accessor method

--- student/student.rb
+++ student/student.rb
@@ -1,4 +1,7 @@
 class Student
+	attr_accessor :id, :phone_number, :git
+	attr_reader :second_name, :first_name, :patronymic
+
 	def initialize(second_name, first_name, patronymic, id=nil, phone_number=nil, git=nil)
 		@second_name = second_name
 		@first_name = first_name
@@ -8,57 +11,8 @@ class Student
 		@git = git
 	end
 
-	def second_name
-		@second_name
-	end
-
-	def second_name=(new_second_name)
-		@second_name = new_second_name
-	end
-
-	def first_name
-		@first_name
-	end
-
-	def first_name=(new_first_name)
-		@first_name = new_second_name
-	end
-
-	def patronymic
-		@patronymic
-	end
-
-	def patronymic=(new_patronymic)
-		@patronymic = new_patronymic
-	end
-
-	def id
-		@id
-	end
-	
-	def id=(new_id)
-		@id = new_id
-	end
-
-	def phone_number
-		@phone_number
-	end
-
-	def phone_number=(new_phone_number)
-		@phone_number = new_phone_number
-	end
-
-	def git
-		@git
-	end
-
-	def git=(new_git)
-		@git = new_git
-	end
-
 	def to_s
   		"ФИО: #{second_name} #{first_name} #{patronymic}, ID: #{id || 'не указан'}, Телефон: #{phone_number || 'не указан'}, Git: #{git || 'не указан'}"
 	end
-
 
 end
