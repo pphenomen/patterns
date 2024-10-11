fc0c5d5c27f88a883e4bd27b59a64da77c646edb
pphenomen <dzamikhovz@gmail.com>
2024-10-12 01:14:33 +0300

    Lab2 - task2.5: refactoring, super class

--- student/student.rb
+++ student/student.rb
@@ -1,16 +1,98 @@
-class Student
-	attr_reader :second_name, :first_name, :patronymic, :id, :phone_number, :git
+class Person
+	attr_reader :id, :git, :phone_number
+
+	def initialize(id: nil, git: nil, phone_number: nil)
+		self.id = id if id
+    	self.git = git if git
+    	self.phone_number = phone_number if phone_number
+	end
+
+	def self.valid_id?(id)
+		id.is_a?(Integer) && id > 0
+	end
+
+	def id=(id)
+		unless Person.valid_id?(id)
+			raise "ID должно быть целым числом больше нуля"
+		end
+		@id = id 
+	end
+
+	def self.clean_phone_number(phone_number)
+    	phone_number.gsub(/\D/, '')
+  	end
+
+  	def self.valid_phone_number?(phone_number)
+    	cleaned_phone = clean_phone_number(phone_number)
+    	cleaned_phone.match?(/\A\d{11}\z/)
+  	end
+
+	def phone_number=(phone_number)
+    	unless Person.valid_phone_number?(phone_number) 
+    		raise "Номер телефона некорректный" 
+    	end
+    	@phone_number = Person.clean_phone_number(phone_number)
+  	end
+  	
+  	def self.valid_git?(git)
+  		git.match?(/\Agithub\.com\/[a-zA-Z0-9_-]+\z/)
+  	end
+
+  	def git=(git)
+  		unless Person.valid_git?(git)
+  			raise "Git некорректный. Пример: github.com/username"
+  		end
+  		@git = git 
+  	end
+
+  	def git_present?
+  		!git.nil?
+	end
+
+	def contact_present?
+		!phone_number.nil?
+	end  
+
+	def validate
+		unless git_present?
+			raise "Git отсутствует"
+		end
+
+		unless contact_present?
+			raise "Необходимо указать номер телефона для связи"
+		end
+	end
+
+	def git_link
+		git || "Git отсутствует"
+	end
+
+	def contact
+		if phone_number
+    		"(номер телефона): #{phone_number}"
+		else 
+			"Контакт отсутствует"
+		end
+	end
+
+	def set_contacts(hash_params={})
+		self.phone_number = hash_params[:phone_number]  if hash_params[:phone_number]
+	end
+
+	def to_s
+		"ID: #{id || 'не указан'}, Телефон: #{phone_number || 'не указан'}, Git: #{git || 'не указан'}"
+	end
+end
+
+class Student < Person
+	attr_reader :second_name, :first_name, :patronymic
 
 	def initialize(hash_params = {})
 		self.second_name = hash_params[:second_name] 
 		self.first_name = hash_params[:first_name]
 		self.patronymic = hash_params[:patronymic]
-		self.id = hash_params[:id] if hash_params[:id]
-		self.git = hash_params[:git] if hash_params[:git]
-	end
+		super(id: hash_params[:id], git: hash_params[:git], phone_number: hash_params[:phone_number])
 
-	def set_contacts(hash_params={})
-		self.phone_number = hash_params[:phone_number]  if hash_params[:phone_number]
 	end
 
 	def self.valid_fullname?(fullname)
@@ -38,90 +120,25 @@ class Student
 		@patronymic = patronymic
 	end
 
-	def self.valid_id?(id)
-		id.is_a?(Integer) && id > 0
-	end
-
-	def id=(id)
-		unless Student.valid_id?(id)
-			raise "ID должно быть целым числом больше нуля"
-		end
-		@id = id 
-	end
-
-	def self.valid_phone_number?(phone_number)
-		phone_number.match?(/\A\d{11}\z/)
-  	end
-
-  	private def phone_number=(phone_number)
-    	unless Student.valid_phone_number?(phone_number) 
-    		raise "Номер телефона некорректный" 
-    	end
-    	@phone_number = phone_number
-  	end
-  	
-  	def self.valid_git?(git)
-  		git.match?(/\Agithub\.com\/[a-zA-Z0-9_-]+\z/)
-  	end
-
-  	def git=(git)
-  		unless Student.valid_git?(git)
-  			raise "Git некорректный. Пример: github.com/username"
-  		end
-  		@git = git 
-  	end
-
-  	def git_present?
-  		!git.nil?
-	end
-
-	def contact_present?
-		!phone_number.nil?
-	end  
-
-	def validate
-		unless git_present?
-			raise "Git отсутствует"
-		end
-
-		unless contact_present?
-			raise "Необходимо указать номер телефона для связи"
-		end
-	end
-
-	def to_s
-  		"ФИО: #{second_name} #{first_name} #{patronymic}, ID: #{id || 'не указан'}, Телефон: #{phone_number || 'не указан'}, Git: #{git || 'не указан'}"
-	end
-
 	def initials
 		"#{second_name} #{first_name[0]}.#{patronymic[0]}."
-	end
-
-	def git_link
-		git || "Git отсутствует"
-	end
-
-	def contact
-		if phone_number
-			"(номер телефона): #{phone_number}"
-		else 
-			"Контакт отсутствует"
-		end
 	end
 
 	def get_info
 		"Фамилия И.О.: #{initials}; Git: #{git_link}, Связь#{contact}"
 	end
+
+	def to_s
+		"ФИО: #{second_name} #{first_name} #{patronymic}, " + super
+	end
 end
 
-class StudentShort
-	attr_reader :id, :surname_initials, :git, :contact 
+class StudentShort < Person
+	attr_reader :surname_initials
 
 	def initialize(id, surname_initials, git, contact)
-		@id = id
     	@surname_initials = surname_initials
-    	@git = git
-    	@contact = contact
+    	super(id: id, git: git, phone_number: contact)
 	end
 
 	def self.from_student(student)
@@ -137,6 +154,6 @@ class StudentShort
 	end
 
 	def to_s
-    	"ID: #{@id}, Фамилия И.О.: #{@surname_initials}, Git: #{@git}, Контакт: #{@contact}"
+    	"ID: #{id}, Фамилия И.О.: #{surname_initials}, Git: #{git}, Контакт: #{contact}"
   	end
 end	
