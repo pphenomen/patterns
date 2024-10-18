e824152750adb9e86f476aecf363eebdf315cd4b
pphenomen <dzamikhovz@gmail.com>
2024-10-18 23:57:16 +0300

    Lab2: refactoring code, splitting classes into files

--- student/main.rb
+++ student/main.rb
@@ -1,20 +1,27 @@
+require './person.rb'
 require './student.rb'
+require './student_short.rb'
 
-student = Student.new({
-	second_name: "Иванов", 
-	first_name: "Иван", 
-	patronymic: "Иванович", 
-	id: 1,
-	git: "github.com/ivanov"
-})
-student.set_contacts({phone_number: "89123456789"})
+student = Student.new(
+  "Иванов", 
+  "Иван", 
+  "Иванович", 
+  id: 1,
+  git: "https://github.com/ivanov",
+  phone_number: "89123456789",
+  email: "ivanov@mail.com",
+  telegram: "ivanov123"
+)
 
-puts("Информация о студентах:")
+puts("Информация о студенте:")
 puts student.to_s
+
+puts("Корректные данные?")
+puts student.validate?
 
 puts("Короткая информация:")
 student1_short = StudentShort.from_student(student)
 puts student1_short.to_s
 
-student2_short = StudentShort.from_string(2, "Петров П.В., github.com/petrov, (номер телефона): 89993334455")
+student2_short = StudentShort.from_string(2, "Петров П.В., https://github.com/petrov, 89993334455")
 puts student2_short.to_s
---
+++ student/person.rb
@@ -0,0 +1,34 @@
+class Person
+	attr_reader :id, :git
+
+	GIT_REGEX = /\Ahttps?:\/\/github\.com\/[a-zA-Z0-9_-]+\z/
+
+	def initialize(id: nil, git: nil)
+		self.id = id if id
+    	self.git = git if git
+	end
+
+	# setters 
+	def id=(id)
+		unless Person.valid_id?(id)
+			raise "ID должно быть целым числом больше нуля"
+		end
+		@id = id 
+	end
+
+  	def git=(git)
+  		unless Person.valid_git?(git)
+  			raise "Git некорректный. Пример: https://github.com/username"
+  		end
+  		@git = git 
+  	end
+
+  	# validators 
+	def self.valid_id?(id)
+		id.is_a?(Integer) && id > 0
+	end
+
+	def self.valid_git?(git)
+  		git.match?(GIT_REGEX)
+  	end
+end
