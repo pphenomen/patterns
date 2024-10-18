16ee89f76c0e87d3f0e89c81a61f5a69969e2348
pphenomen <dzamikhovz@gmail.com>
2024-10-18 23:57:16 +0300

    Lab2 - refactoring code, splitting classes into files

--- student/student.rb
+++ student/student.rb
@@ -1,102 +1,52 @@
-class Person
-	attr_reader :id, :git, :phone_number
-
-	def initialize(id: nil, git: nil, phone_number: nil)
-		self.id = id if id
-    	self.git = git if git
-    	self.phone_number = phone_number if phone_number
-	end
-
-	def self.valid_id?(id)
-		id.is_a?(Integer) && id > 0
-	end
-
-	def id=(id)
-		unless Person.valid_id?(id)
-			raise "ID должно быть целым числом больше нуля"
-		end
-		@id = id 
-	end
-
-	def self.clean_phone_number(phone_number)
-    	phone_number.gsub(/\D/, '')
-  	end
-
-  	def self.valid_phone_number?(phone_number)
-    	cleaned_phone = clean_phone_number(phone_number)
-    	cleaned_phone.match?(/\A\d{11}\z/)
-  	end
-
-	def phone_number=(phone_number)
-    	unless Person.valid_phone_number?(phone_number) 
-    		raise "Номер телефона некорректный" 
-    	end
-    	@phone_number = Person.clean_phone_number(phone_number)
-  	end
-  	
-  	def self.valid_git?(git)
-  		git.match?(/\Agithub\.com\/[a-zA-Z0-9_-]+\z/)
-  	end
-
-  	def git=(git)
-  		unless Person.valid_git?(git)
-  			raise "Git некорректный. Пример: github.com/username"
-  		end
-  		@git = git 
-  	end
-
-  	def git_present?
-  		!git.nil?
-	end
-
-	def contact_present?
-		!phone_number.nil?
-	end  
-
-	def validate
-		unless git_present?
-			raise "Git отсутствует"
-		end
-
-		unless contact_present?
-			raise "Необходимо указать номер телефона для связи"
-		end
-	end
-
-	def git_link
-		git || "Git отсутствует"
-	end
-
-	def contact
-		if phone_number
-    		"(номер телефона): #{phone_number}"
-		else 
-			"Контакт отсутствует"
-		end
-	end
-
-	def set_contacts(hash_params={})
-		self.phone_number = hash_params[:phone_number]  if hash_params[:phone_number]
-	end
-
-	def to_s
-		"ID: #{id || 'не указан'}, Телефон: #{phone_number || 'не указан'}, Git: #{git || 'не указан'}"
-	end
-end
+require './person.rb'
 
 class Student < Person
-	attr_reader :second_name, :first_name, :patronymic
+	attr_reader :second_name, :first_name, :patronymic, :phone_number, :email, :telegram
 
-	def initialize(hash_params = {})
-		self.second_name = hash_params[:second_name] 
-		self.first_name = hash_params[:first_name]
-		self.patronymic = hash_params[:patronymic]
-		super(id: hash_params[:id], git: hash_params[:git], phone_number: hash_params[:phone_number])
+	PHONE_REGEX = /\A\d{11}\z/
+  	EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  	TELEGRAM_REGEX = /\A[a-zA-Z0-9_]{5,32}\z/
+  	FULLNAME_REGEX = /\A[А-Яа-яЁёA-Za-z]+\z/
 
+	def initialize(second_name, first_name, patronymic, hash_params = {id: nil, git: nil, phone_number: nil, email: nil, telegram: nil})
+		self.second_name = second_name
+		self.first_name = first_name
+		self.patronymic = patronymic
+		super(id: hash_params[:id], git: hash_params[:git])
+		self.set_contacts(phone_number: hash_params[:phone_number], email: hash_params[:email], telegram: hash_params[:telegram])
 	end
 
+	def set_contacts(phone_number: nil, email: nil, telegram: nil)
+		unless phone_number.nil? || Student.valid_phone_number?(phone_number)
+      		raise "Номер телефона некорректный"
+    	end
+    	@phone_number = phone_number
+
+    	unless email.nil? || Student.valid_email?(email)
+      		raise "Адрес почты некорректный"
+   		end
+    	@email = email
+
+    	unless telegram.nil? || Student.valid_telegram?(telegram)
+      		raise "Имя пользователя Telegram некорректное"
+    	end
+    	@telegram = telegram
+	end
+
+	def self.valid_phone_number?(phone_number)
+  		phone_number.match?(PHONE_REGEX)
+  	end
+
+  	def self.valid_email?(email)
+  		email.match?(EMAIL_REGEX)
+  	end
+
+  	def self.valid_telegram?(telegram)
+  		telegram.match?(TELEGRAM_REGEX)
+  	end
+
 	def self.valid_fullname?(fullname)
-		!fullname.nil? && fullname.match?(/\A[А-Яа-яЁёA-Za-z]+\z/)
+		!fullname.nil? && fullname.match?(FULLNAME_REGEX)
 	end
 
 	def second_name=(second_name)
@@ -120,40 +70,36 @@ class Student < Person
 		@patronymic = patronymic
 	end
 
+  	def git_present?
+  		!git.nil?
+	end
+
+	def contact_present?
+		!phone_number.nil? || !email.nil? || !telegram.nil?
+	end  
+
+	def validate?
+		git_present? && contact_present?
+	end
+
+	def contact
+		contacts = []
+    	contacts << "номер телефона: #{phone_number}" if phone_number
+    	contacts << "почта: #{email}" if email
+    	contacts << "телеграм: #{telegram}" if telegram
+    	contacts.empty? ? "Нет контактной информации" : contacts.join(", ")
+	end
+
 	def initials
 		"#{second_name} #{first_name[0]}.#{patronymic[0]}."
 	end
 
 	def get_info
-		"Фамилия И.О.: #{initials}; Git: #{git_link}, Связь#{contact}"
+		"Фамилия И.О.: #{initials}; Git: #{git}, Связь: #{contact}"
 	end
 
 	def to_s
-		"ФИО: #{second_name} #{first_name} #{patronymic}, " + super
+		"ФИО: #{second_name} #{first_name} #{patronymic}, ID: #{id}, Git: #{git}, Контактная информация: #{contact}"
 	end
 end
 
-class StudentShort < Person
-	attr_reader :surname_initials
-
-	def initialize(id, surname_initials, git, contact)
-    	@surname_initials = surname_initials
-    	super(id: id, git: git, phone_number: contact)
-	end
-
-	def self.from_student(student)
-		new(student.id, student.initials, student.git_link, student.contact)
-	end
-
-	def self.from_string(id, info_string)
-		info = info_string.split(", ")
-		surname_initials = info[0]
-		git = info[1] || "Git отсутствует"
-		contact = info[2] || "Контакт отсутствует"
-		new(id, surname_initials, git, contact) 
-	end
-
-	def to_s
-    	"ID: #{id}, Фамилия И.О.: #{surname_initials}, Git: #{git}, Контакт: #{contact}"
-  	end
-end	
---
+++ student/student_short.rb
@@ -0,0 +1,27 @@
+require './person.rb'
+
+class StudentShort < Person
+	attr_reader :surname_initials, :contact
+
+	def initialize(surname_initials, id:nil, git:nil, contact:nil)
+    	@surname_initials = surname_initials
+    	@contact = contact
+    	super(id: id, git: git)
+	end
+
+	def self.from_student(student)
+		new(student.initials, id: student.id, git: student.git, contact: student.contact)
+	end
+
+	def self.from_string(id, info_string)
+		info = info_string.split(", ")
+		surname_initials = info[0]
+		git = info[1] 
+		contact = info[2]
+		new(surname_initials, id: id, git: git, contact: contact)
+	end
+
+	def to_s
+    	"ID: #{id}, Фамилия И.О.: #{surname_initials}, Git: #{git}, Контактная информация: #{contact}"
+  	end
+end	
