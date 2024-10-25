5d5d50208272a5a40fe115295a40867a88215096
pphenomen <dzamikhovz@gmail.com>
2024-10-25 23:25:06 +0300

    Lab3 - task2: all? method

--- polymorphism_blocks/array_processor.rb
+++ polymorphism_blocks/array_processor.rb
@@ -1,8 +1,16 @@
 # Вариант 2
 class ArrayProcessor
-	attr_reader	:arr
+	attr_reader: arr
 
 	def initialize(arr)
 		@arr = arr.freeze
+	end
+
+	# 2. all?
+	def all?(&block)
+		@arr.each do |element|
+			return false unless block.call(element)
+		end
+		true
 	end
 end
