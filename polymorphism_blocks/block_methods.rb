5290a187d2d203c98cecc0e098f3660cadc23e88
pphenomen <dzamikhovz@gmail.com>
2024-10-25 17:04:10 +0300

    Lab3 - task1: count between mins method

--- polymorphism_blocks/block_methods.rb
+++ polymorphism_blocks/block_methods.rb
@@ -7,3 +7,13 @@ end
 def count_in_interval(arr, a, b)
 	arr.count { |num| num.between?(a,b) }
 end
+
+# 1.26 Дан целочисленный массив. Необходимо найти количество элементов между первым и последним минимальным.
+def count_between_mins(arr)
+	min_value = arr.min
+	first_min_index = arr.index(min_value)
+	last_min_index = arr.index(min_value)
+	return 0 if first_min_index == last_min_index 
+
+	arr[first_min_index + 1...last_min_index].size
+end
