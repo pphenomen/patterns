97719b27f5a950c255fbc06153fd3a7259f1f4d2
pphenomen <dzamikhovz@gmail.com>
2024-10-25 17:03:26 +0300

    Lab3 - task1: count in interval method

--- polymorphism_blocks/block_methods.rb
+++ polymorphism_blocks/block_methods.rb
@@ -2,3 +2,8 @@
 def find_min_index(arr)
 	arr.index(arr.min)
 end
+
+# 1.14 Дан целочисленный массив и интервал a..b. Необходимо найти количество элементов в этом интервале.
+def count_in_interval(arr, a, b)
+	arr.count { |num| num.between?(a,b) }
+end
