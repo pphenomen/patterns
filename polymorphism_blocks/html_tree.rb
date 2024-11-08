2bd2c00aff7c97c93e0d275ef43b9e8722233eb5
pphenomen <dzamikhovz@gmail.com>
2024-11-09 00:48:23 +0300

    Lab3: fixed block method, improved html tree

--- polymorphism_blocks/html_tree.rb
+++ polymorphism_blocks/html_tree.rb
@@ -1,3 +1,47 @@
+class Tree
+	include Enumerable
+
+	attr_accessor :root
+
+	def initialize(root = nil)
+		@root = root
+	end
+
+	def each(&block)
+	    dfs(&block)
+	end
+
+	def bfs
+		return if @root.nil?
+
+	    queue = [@root]
+	    while queue.any?
+	    	current_node = queue.shift
+	      	yield(current_node)
+	     	queue.concat(current_node.children)
+	    end
+	end
+
+	def dfs
+	    return if @root.nil?
+
+	    stack = [@root]
+	    while stack.any?
+	    	current_node = stack.pop
+	    	yield(current_node)
+	    	current_node.children.reverse.each { |child| stack.push(child) } # reverse для сохранения порядка
+	    end
+	end
+
+	def add_child(node, parent = nil)
+	    if parent.nil?
+	    	@root = node
+	    else
+	      	parent.add_child(node)
+	    end
+	end
+end
+
 class HtmlTag
 	attr_reader :tag_name, :attributes, :children
 	attr_accessor :content 
@@ -46,86 +90,61 @@ class HtmlTag
 	end
 end
 
-class HtmlTree
-	include Enumerable
+class HtmlTree < Tree
 
-	attr_accessor :root
+  	def initialize(html)
+    	super()
+    	parse_html(html)
+  	end
 
-	def initialize(html)
-		@root = parse_html(html)
-	end
+  	def parse_html(html)
+    	stack = []
+    	current_node = nil
 
-	def parse_html(html)
-		stack = []
-		current_node = nil
+    	html.scan(/<\/?(\w+)(.*?)\/?>/) do |tag_open, attrs|
+	      	if tag_open
+	        	attributes = parse_attributes(attrs.strip)
+	        	new_node = HtmlTag.new(tag_name: tag_open, attributes: attributes)
 
-		html.scan(/<\/?(\w+)(.*?)\/?>/) do |tag_open, attrs|
-			if tag_open
-				attributes = parse_attributes(attrs.strip)
-				new_node = HtmlTag.new(tag_name: tag_open, attributes: attributes)
+	        	if current_node.nil?
+	          		current_node = new_node
+	          		@root = current_node
+	        	else
+	          		current_node.add_child(new_node)
+	          		stack.push(current_node)
+	          		current_node = new_node
+	        	end
+	      	else
+	        	current_node = stack.pop unless stack.empty?
+	      	end
+    	end
+  	end
 
-				if current_node.nil?
-					current_node = new_node
-					@root = current_node
-				else
-					current_node.add_child(new_node)
-					stack.push(current_node)
-          			current_node = new_node
-				end
-			else
-				current_node = stack.pop unless stack.empty?
-			end
-		end
+  	def parse_attributes(attributes_string)
+    	attributes_string.scan(/(\w+)=["'](.*?)["']/).to_h
+  	end
 
-		@root
-	end
-
-	def parse_attributes(attributes_string)
-		attributes_string.scan(/(\w+)=["'](.*?)["']/).to_h
-	end 
-
-	def bfs
-		return if @root.nil?
-
-		queue = [@root]
-		while queue.any?
-			current_node = queue.shift
-			yield(current_node)
-			queue.concat(current_node.children)
-		end
-	end
-
-	def dfs
-		return if @root.nil?
-
-		stack = [@root]
-		while stack.any?
-			current_node = stack.pop
-			yield(current_node)
-			current_node.children.reverse.each { |child| stack.push(child) } # reverse для сохранения порядка
-		end
-	end
-
-	def each_bfs(&block)
-		bfs(&block)
- 	 end
-
-  	def each_dfs(&block)
-    	dfs(&block)
+  	def to_s
+    	@root.to_s
   	end
 end
 
-html = '<div class="container"><p>Hello</p><span>World</span></div><div class="container"><p>Hello</p><span>World</span></div>'
-tree = HtmlTree.new(html)
+html = "<html><body><h1 class='title'>Hello, World!</h1><p>Welcome to the club buddy</p></body></html>"
+html_tree = HtmlTree.new(html)
+
+puts html_tree
 
 puts "\nHTML tree (BFS):"
-tree.each_bfs { |node| puts node.to_s }
+html_tree.bfs { |node| puts node.tag_name }
 
 puts "\n\nHTML tree (DFS):"
-tree.each_dfs { |node| puts node.to_s }
+html_tree.dfs { |node| puts node.tag_name }
+
+puts "\neach(DFS):"
+html_tree.each { |node| puts node.tag_name }
 
 puts "\nCount children for each node:"
-tree.each_dfs { |node| puts "#{ngode.tag_name} has #{node.count_children} child(ren)." }
+html_tree.each { |node| puts "#{node.tag_name} has #{node.count_children} child(ren)." }
 
 puts "\nHas children?"
-tree.each_dfs { |node| puts "#{node.tag_name} has children: #{node.has_children?}" }
+html_tree.each { |node| puts "#{node.tag_name} has children: #{node.has_children?}" }
