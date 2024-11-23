076ad9aa7f179479f9ecc9c8815ddb0211343dcb
pphenomen <dzamikhovz@gmail.com>
2024-11-23 19:05:43 +0300

    Lab3: using enumerable methods for html tree

--- polymorphism_blocks/html_tree/main.rb
+++ polymorphism_blocks/html_tree/main.rb
@@ -15,3 +15,10 @@ html_tree.dfs { |node| puts "Tag: #{node.tag_name}, Content: #{node.content}" }
 
 puts "\nEach(DFS):"
 html_tree.each { |node| puts "Tag: #{node.tag_name}" }
+
+puts "\nSelect:"
+selected_nodes = html_tree.select { |node| node.tag_name == 'p' }
+selected_nodes.each { |node| puts "Tag: #{node.tag_name}, Content: #{node.content}" }
+
+puts "\nAll tags are 'div'?:"
+puts html_tree.all? { |node| node.tag_name == 'div' }
