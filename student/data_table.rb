0b31e930d71f45e78a86b3329630bc0003dd1427
pphenomen <dzamikhovz@gmail.com>
2024-11-28 22:36:49 +0300

    Lab4-task1.2: methods get_element, rows_count, columns_count

--- student/data_table.rb
+++ student/data_table.rb
@@ -9,6 +9,21 @@ class DataTable
 		unless data.is_a?(Array) && data.all? { |row| row.is_a?(Array) }
 			raise ArgumentError, "Объект должен являться двумерным массивом"
 		end
-		@data = data
+		@data = data.map(&:dup)
+	end
+
+	def rows_count
+		data.size
+	end
+
+	def columns_count
+		data[0].size
+	end
+
+	def get_element(row,col)
+		raise IndexError, "Неверный индекс строки" if row < 0 || row >= rows_count
+		raise IndexError, "Неверный индекс столбца" if col < 0 || col >= columns_count
+
+		data[row][col]
 	end
 end
--- student/main.rb
+++ student/main.rb
@@ -2,6 +2,7 @@ require './person.rb'
 require './student.rb'
 require './student_short.rb'
 require './student_binary_tree.rb'
+require './data_table.rb'
 
 student1 = Student.new(
 	second_name: "Иванов", 
@@ -39,28 +40,35 @@ student3 = Student.new(
 	telegram: "vasilyev123"
 )
 
-puts("Информация о студентах:")
-puts student1
-puts student2
-puts student3
+# puts("Информация о студентах:")
+# puts student1
+# puts student2
+# puts student3
 
-puts("Корректные данные?")
-puts student1.validate?
-puts student2.validate?
-puts student3.validate?
+# puts("Корректные данные?")
+# puts student1.validate?
+# puts student2.validate?
+# puts student3.validate?
 
-puts("Короткая информация о студенте 1:")
-student1_short = StudentShort.from_student(student1)
-puts student1_short
+# puts("Короткая информация о студенте 1:")
+# student1_short = StudentShort.from_student(student1)
+# puts student1_short
 
-puts("Короткая информация о студенте 2 из строки:")
-student2_short = StudentShort.from_string(2, "Петров П.В., https://github.com/petrov, 89993334455") 
-puts student2_short
+# puts("Короткая информация о студенте 2 из строки:")
+# student2_short = StudentShort.from_string(2, "Петров П.В., https://github.com/petrov, 89993334455") 
+# puts student2_short
 
-tree = StudentBinaryTree.new
-tree.insert(student1)
-tree.insert(student2)
-tree.insert(student3)
+# tree = StudentBinaryTree.new
+# tree.insert(student1)
+# tree.insert(student2)
+# tree.insert(student3)
 
-puts "\nОтсортированные даты рождения студентов по возрастанию:"
-tree.each { |student| puts student }
+# puts "\nОтсортированные даты рождения студентов по возрастанию:"
+# tree.each { |student| puts student }
+
+testdata = DataTable.new([[1,2,3],[4,5,6]])
+
+puts testdata.inspect
+puts testdata.rows_count
+puts testdata.columns_count
+puts testdata.get_element(1,2)
