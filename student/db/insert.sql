237ab514b9cb6973ccafa92a94402e4c0448d843
pphenomen <dzamikhovz@gmail.com>
2024-12-04 14:45:59 +0300

    Lab4: some fixes

--- student/db/insert.sql
+++ student/db/insert.sql
@@ -1,5 +1,7 @@
 INSERT INTO student (second_name, first_name, patronymic, birthdate, phone_number, email, telegram, git)
 VALUES
-    ('Иванов', 'Иван', 'Иванович', '06.05.1995', '89161234567', 'ivanov@example.com', 'ivanov_ivan', 'https://github.com/ivanov_git'),
-    ('Петров', 'Петр', 'Петрович', '02.10.1998', '89162223344', 'petrov@example.com', 'petrov_petr', 'https://github.com/petrov_git'),
-    ('Смирнова', 'Мария', 'Ивановна', '09.03.1997', '89164445566', 'smirnova@example.com', 'smirnova_maria', 'https://github.com/smirnova_git')
+    ('Иванов', 'Иван', 'Иванович', '1995-05-06', '89161234567', 'ivanov@example.com', 'ivanov_ivan', 'https://github.com/ivanov_git'),
+    ('Петров', 'Петр', 'Петрович', '1998-10-02', '89162223344', 'petrov@example.com', 'petrov_petr', 'https://github.com/petrov_git'),
+    ('Смирнова', 'Мария', 'Ивановна', '1997-03-09', '89164445566', 'smirnova@example.com', 'smirnova_maria', 'https://github.com/smirnova_git'),
+    ('Сергеев', 'Сергей', 'Владимирович', '2001-07-15', '89231234567', 'sergeev@example.com', 'sergeev_serg', 'https://github.com/sergeev_git'),
+    ('Кузнецов', 'Алексей', 'Андреевич', '2000-12-21', '89312223344', 'kuznetsov@example.com', 'alex_kuz', 'https://github.com/kuznetsov_git');
