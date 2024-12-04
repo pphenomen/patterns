ba007d5be00cda3fe0fe4b7856e70b7cdd18b45a
pphenomen <dzamikhovz@gmail.com>
2024-12-04 16:01:59 +0300

    Lab4-task3.5: select from ruby

--- student/db/connection.rb
+++ student/db/connection.rb
@@ -1,7 +1,7 @@
 require 'pg'
 
 begin
-	con = PG.connect(
+	connection = PG.connect(
 		dbname: 'StudentsDB',
 		user: 'postgres',
 		password: '0311',
@@ -9,7 +9,15 @@ begin
 		port: 5432
 		)
 	puts "Соединение с базой установлено"
-	con.close
+
+	result = connection.exec("SELECT * FROM student;")
+	result.each do |row|
+		puts "ID: #{row['id']}, ФИО: #{row['second_name']} #{row['first_name']} #{row['patronymic']}, Дата рождения: #{row['birthdate']}, 
+		Телефон: #{row['phone_number']}, Email: #{row['email']}, Telegram: #{row['telegram']}, Git: #{row['git']}"
+  	end
+
 rescue PG::Error => e
-	puts "Ошибка подключения: #{e.message}"
+	puts "Ошибка подключения или выполнения запроса: #{e.message}"
+ensure
+	connection.close if connection
 end
